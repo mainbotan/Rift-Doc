72516e6c10985105c95950ea1e30754679127cce
mainbotan <ned.serafim@gmail.com>
2025-05-12 20:16:34 +0300

    video page in process

--- src/pages/Video/index.tsx
+++ src/pages/Video/index.tsx
@@ -7,6 +7,7 @@ import styles from './styles.module.scss';
 import { VideoCard } from '../../ui-kit/VideoCard'; // <- video card from ui-
 import { ModelCard } from '../../ui-kit/ModelCard'; // <- model card from ui-kit
 import { Tag } from '../../ui-kit/Tag'; // <- tag from ui-kit
+import { Button } from '../../ui-kit/Button'; // <- button from ui-kit
 import { CategoryCard } from '../../ui-kit/CategoryCard'; // <- category card from ui-kit
 
 type Props = {
@@ -27,6 +28,58 @@ export const VideoPage = ({ }: Props) => (
     <div className={ styles.info }>
       <div className={ styles.playerArea }>
 
+      </div>
+      <div className={ styles.videoInfoArea }>
+        <div className={ styles.titleArea }>asddddddddddddddddddddd</div>
+        <div className={ styles.actionArea }>
+          <div className={ styles.item }></div>
+        </div>
+      </div>
+      <div className={ styles.creatorArea }>
+        <div className={ styles.avatarArea }>
+          <div className={ styles.avatar }></div>
+        </div>
+        <div className={ styles.infoArea }>
+          <div className={ styles.name }>фывввввввв</div>
+          <div className={ styles.description }>
+            фывфывфывфыв
+          </div>
+        </div>
+        <div className={ styles.actionArea }>
+          <div className={ styles.item }>
+            <Button 
+              text='Share'
+              size='bg'
+              color='default'
+            />
+          </div>
+          <div className={ styles.item }>
+            <Button 
+              text='Subscribe'
+              size='bg'
+              color='contrast'
+            />
+          </div>
+        </div>
+      </div>
+      <div className={ styles.tagsArea }>
+        <div className={ styles.item }>
+          <Tag
+            text='gay'
+            size='md'
+            color='default'
+          />
+        </div>
+      </div>
+      <div className={ styles.featuredArea }>
+        <div className={ styles.item }>
+          <ModelCard
+            name='ivan zolo 2004'
+            img=''
+            size='md'
+            color='empty'
+          />
+        </div>
       </div>
     </div>
     <div className={ styles.recommendations }>

bb2a49596a553216d1d3bd8fc76f25366c6743f2
mainbotan <ned.serafim@gmail.com>
2025-07-27 17:00:19 +0300

    refactor docs layout 2.0

--- src/layouts/Docs/NavBar.tsx
+++ src/layouts/Docs/NavBar.tsx
@@ -1,11 +1,13 @@
 import { useParams } from 'react-router-dom';
 import styles from './styles.module.scss';
+import clsx from 'clsx';
 
 export const NavBar = () => {
   const { version = 'v1' } = useParams();
 
   return (
     <div className={styles.navBar}>
+        <div className={clsx(styles.capture, styles.versionMark)}>version {version}</div>
         <div className={styles.capture}>On this page</div>
     </div>
   );

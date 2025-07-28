240cb3fd3339e95197fcd51c50f6a11e8a835384
mainbotan <ned.serafim@gmail.com>
2025-07-28 17:32:44 +0300

    fix react-router 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 5173, 
+    port: 5173
   },
+  preview: {
+    port: 5173
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html'
+    }
+  }
 })
